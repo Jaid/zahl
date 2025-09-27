600d11234075b1909eae315f98aed5fdfc15dbf1
Jaid <6216144+Jaid@users.noreply.github.com>
2025-09-27 23:10:19 +0200

    Modernized (more Bun, more TypeScript)
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,15 +1,12 @@
-import * as path from 'forward-slash-path'
+import { it, expect } from "bun:test"
 
-const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
+import {getMainModuleDefault} from 'zeug'
 
-/**
- * @type { import("../src") }
- */
-const {default: zahl} = indexModule
+const zahl = await getMainModuleDefault<typeof import('../src/index.ts')>('zahl')
 
 it("should run", () => {
   expect(zahl(3, "Banana")).toBe("3 Bananas")
-  expect(zahl(1, "Banana")).toBe("1 Banana")
+  expect(zahl(1, "ripe banana")).toBe("1 ripe banana")
   expect(zahl(["a", "b"], "car")).toBe("2 cars")
   expect(zahl({
     a: 1,
